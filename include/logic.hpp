c4aa843e33facaccf3f95daf706c3038a2aa4b1f
tailsmonster <aroca.twelve@proton.me>
2025-03-10 16:59:53 -0400

    fixed header

--- include/logic.hpp
+++ include/logic.hpp
@@ -12,13 +12,13 @@ int getLow(int answer);
 
 // // Messages
 void startMsg(int low, int top);
-int inputMsg(int low, int top)
+int inputMsg(int low, int top);
 void hintMsg(int guess, int answer);
 void wrongMsg(int guess, int answer);
 void tauntMsg(int attempts);
 void winMsg(int attempts, int guess);
 
 // // Game
-bool gameloop(int guess, int answer, int attempts);
+bool gameloop(int guess, int answer);
 
 
