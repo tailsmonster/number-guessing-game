f086871dce0de8e6fc0819802cec94839c0da8d5
tailsmonster <aroca.twelve@proton.me>
2025-03-10 16:15:44 -0400

    i think thats it for the header file

--- include/logic.hpp
+++ include/logic.hpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+#include <typeinfo>
 
 // init
 int getTop();
@@ -10,12 +11,14 @@ int getAnswer(int top);
 int getLow(int answer);
 
 // // Messages
-void start_msg(int low, int top);
-void wrong_msg(int attempts);
-void taunt_msg(int attempts);
-void win_msg(int attempts, int guess);
+void startMsg(int low, int top);
+int inputMsg(int low, int top)
+void hintMsg(int guess, int answer);
+void wrongMsg(int guess, int answer);
+void tauntMsg(int attempts);
+void winMsg(int attempts, int guess);
 
 // // Game
-int isCorrect(int guess, int low, int top);
+bool gameloop(int guess, int answer, int attempts);
 
 
